8392ff7b88dbb97de7d9dbe852cf9bb56f592c4d
n2d <nikitos75091@gmail.com>
2022-05-23 17:54:06 +0900

    Выполнил 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("nameFirst","lastNameFirst",(byte)21);
+        userDao.saveUser("nameSecond","lastNameSecond",(byte)22);
+        userDao.saveUser("nameThird","lastNameThird",(byte)23);
+        userDao.saveUser("nameFourth","lastNameFourth",(byte)24);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDao {
@@ -16,4 +17,20 @@ public interface UserDao {
     List<User> getAllUsers();
 
     void cleanUsersTable();
+
+    default void printSQLException(SQLException ex) {
+        for (Throwable e: ex){
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState"+((SQLException)e).getSQLState());
+                System.err.println("Error Code"+((SQLException)e).getErrorCode());
+                System.err.println("message" + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null){
+                    System.out.println("cause"+t);
+                    t=t.getCause();
+                }
+            }
+        }
+    }
 }
